d54e733f84ee3aa3bcdbdb230e46dc4f51498f8c
Pino K Win <pino@kaiser>
2011-06-24 13:47:40 -0700

    initial drop

---
+++ de.vogella.osgi.firstbundle/src/de/vogella/osgi/firstbundle/Activator.java
@@ -0,0 +1,28 @@
+package de.vogella.osgi.firstbundle;
+
+import java.util.Map;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import de.vogella.osgi.firstbundle.internal.MyThread;
+
+public class Activator implements BundleActivator {
+	private MyThread myThread;
+
+	public void start(BundleContext context) throws Exception {
+		Map ss = null;
+		System.out.println("Starting de.vogella.osgi.firstbundle");
+		myThread = new MyThread();
+		myThread.start();
+	}
+
+
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Stopping de.vogella.osgi.firstbundle");
+		myThread.stopThread();
+		myThread.join();
+	}
+
+
+}
